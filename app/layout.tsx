13073d785c1654c4a3a91c9e7c5f64b993ee6409
Adrian Borup <adrian@borup.com>
2024-07-28 11:16:34 +0200

    Center content

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="da" className={GeistSans.className}>
       <body className="bg-background text-foreground">
-        <main className="min-h-screen flex flex-col items-center">
+        <main className="flex justify-center max-w-xl flex-col mx-auto">
           {children}
         </main>
       </body>
