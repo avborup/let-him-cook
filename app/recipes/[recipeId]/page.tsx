a2bc7fb3aa5a24a40ccc82bc454bc4539e20dd74
Adrian Borup <adrian@borup.com>
2024-09-01 14:36:19 +0200

    Use static site generation (SSG) for recipe pages

--- app/recipes/[recipeId]/page.tsx
+++ app/recipes/[recipeId]/page.tsx
@@ -1,23 +1,48 @@
 import RecipeView from "@/components/recipe/recipe-view-server";
-import { createClient } from "@/utils/supabase/server";
+import { createClient } from "@/utils/supabase/client";
+import { unstable_cache } from "next/cache";
 import { notFound } from "next/navigation";
+
+export const revalidate = 3600; // invalidate every hour
 
 export default async function RecipePage({
   params,
 }: {
   params: { recipeId: string };
 }) {
-  const supabase = createClient();
-  const { data } = await supabase
-    .from("recipes")
-    .select()
-    .eq("id", params.recipeId)
-    .limit(1)
-    .single();
+  const getRecipe = unstable_cache(
+    async () => {
+      const supabase = createClient();
+
+      const { data } = await supabase
+        .from("recipes")
+        .select()
+        .eq("id", params.recipeId)
+        .limit(1)
+        .single();
+
+      return data;
+    },
+    ["recipe", params.recipeId],
+    {
+      revalidate: 3600,
+      tags: ["recipe"],
+    },
+  );
+
+  const data = await getRecipe();
 
   if (!data) {
     notFound();
   }
 
   return <RecipeView data={data} />;
+}
+
+export async function generateStaticParams() {
+  const supabase = createClient();
+
+  const results = await supabase.from("recipes").select("id");
+
+  return (results.data ?? []).map(({ id }) => ({ recipeId: id }));
 }
